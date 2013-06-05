29cb0bf4d1d5fbe20130a9432c6b484fee64c88f
Matteo Simone <matteo.j.simone@gmail.com>
2013-06-05 11:14:10 -0700

    action bar fixes

--- Grubber/src/com/example/grubber/FoodPageActivity.java
+++ Grubber/src/com/example/grubber/FoodPageActivity.java
@@ -28,13 +28,17 @@ import com.google.gson.reflect.TypeToken;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -158,8 +162,7 @@ public class FoodPageActivity extends Activity implements View.OnClickListener {
 		voteComment = (EditText) findViewById(R.id.foodpage_commentET);
 		reviewLV = (ListView) findViewById(R.id.foogpage_reviewList);
 		//reviewMoreTV = (TextView) findViewById(R.id.foodpage_reviewMoreTV);
-		
-		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		
 		if(SaveSharedPreference.getUserId(context) == 0){
 	  		voteBtn.setClickable(false);
@@ -200,12 +203,70 @@ public class FoodPageActivity extends Activity implements View.OnClickListener {
 			  } 
 		});
 	}
+	
+	public void onResume() {
+    	super.onResume();
+    	//Refresh the options menu when this activity comes in focus
+    	invalidateOptionsMenu();
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_food_page, menu);
-		return true;
+		//Change profile button to login/register if they are not logged in
+        if(SaveSharedPreference.getUserId(FoodPageActivity.this) == 0)
+        {
+            MenuItem profileItem = menu.findItem(R.id.action_profile);
+        	profileItem.setTitle(R.string.login);
+            //Toast.makeText(this,"Not logged in",Toast.LENGTH_SHORT).show();
+        }
+        else {
+        	MenuItem signout = menu.findItem(R.id.action_signout);
+        	signout.setVisible(true);
+            signout.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+            	public boolean onMenuItemClick(MenuItem item) {            		        	
+        			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        			alertDialogBuilder.setTitle(R.string.logout_msg);
+        			alertDialogBuilder.setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+        				public void onClick(DialogInterface dialog,int id) {            					    							
+							//int tempUserName = SaveSharedPreference.getUserId(context);    			        		
+			        		dialog.cancel();    			        		
+			        		SaveSharedPreference.setUserId(context, 0);
+        					Toast.makeText(context , "Logged out" , Toast.LENGTH_SHORT).show();
+        					invalidateOptionsMenu();
+						}    						
+					}).setNegativeButton("No", new DialogInterface.OnClickListener() {
+    					public void onClick(DialogInterface dialog,int id) {    						
+    						dialog.cancel();    					
+    					}}
+    				  );            		
+            		AlertDialog alertDialog = alertDialogBuilder.create();
+            		alertDialog.show();   
+            		return true;            		
+            	} 	
+            });        	
+        }        
+        return true;
+    }
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_profile:
+	    	  if(SaveSharedPreference.getUserId(FoodPageActivity.this) != 0){
+	    		  Intent intent3 = new Intent(context, ProfileActivity.class);
+	    		  startActivity(intent3);   
+	    	  } else {
+	    		  Intent intent3 = new Intent(context, SignInTabsActivity.class);
+	    		  startActivity(intent3);   
+	    	  }
+	          break; 
+		case android.R.id.home:
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 	
 	@Override
